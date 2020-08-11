6771c54327d9252f0b0e4753c54e5e323bc6bbf0
Benjamin Akar <benjaminakar2001@gmail.com>
2020-08-11 23:49:52 +0200

    No more :any because that's bad

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,18 @@
-import Document from 'next/document';
+import Document, { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { ReactElement } from 'react';
+import { NextPageContext } from 'next';
+import { Context } from 'vm';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
