41de2117f5f27518271dd1000a5a4c8b5ee67055
Benjamin Akar <benjaminakar2001@gmail.com>
2020-12-26 03:41:32 +0100

    fix next.js build

--- src/ui/layouts/parts/Footer.tsx
+++ src/ui/layouts/parts/Footer.tsx
@@ -4,9 +4,7 @@ import styled from 'styled-components';
 export default function Footer() {
   return (
     <Container>
-      <Link href={'https://twitter.com/benjaminakar'}>
-        Made by Benjamin Akar
-      </Link>
+      <Link href={'https://twitter.com/benjaminakar'}>Made by Benjamin Akar</Link>
     </Container>
   );
 }
@@ -15,7 +13,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  background: ${(props) => props.theme.footerBackground};
   padding: 10px 0;
 `;
 
@@ -23,11 +20,10 @@ const Link = styled.a`
   text-decoration: none;
   font-family: 'Open Sans', sans-serif;
   font-size: 13px;
-  color: ${(props) => props.theme.text};
+  color: ${props => props.theme.text};
 
   transition: 0.2s all ease-in-out;
   &:hover {
     opacity: 0.8;
-    color: ${(props) => props.theme.primary};
   }
 `;
